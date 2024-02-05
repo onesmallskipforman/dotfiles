41814875dd0d6f8a12dbeca8bbf11127ebe7cf5b
onesmallskipforman <skipper.gonzalez@icloud.com>
2024-02-05 14:40:08 -0500

    Add initial configs for firefox+thunderbird, and update nvim, picom, polybar, rofi, and bspwm themeing

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -4,3 +4,19 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("extensions.webextensions.ExtensionStorageIDB.migrated.addon@darkreader.org", true);
 user_pref("extensions.webextensions.ExtensionStorageIDB.migrated.uBlock0@raymondhill.net", true);
 // user_pref("xpinstall.signatures.required", false); // don't think this works unless i get ffx dev edition or nightly
+
+
+user_pref("browser.tabs.tabmanager.enabled", false); // remove tab manager button
+
+// 0: Start with a blank page (about:blank).
+// 1: Start with the web page(s) defined as the home page(s). (Default)
+// 2: Load the last visited page.
+// 3: Resume the previous browser session
+user_pref("browser.startup.page", 3);
+
+
+user_pref("browser.startup.homepage", "about:blank"); // TODO: using about:blank doesn't use usercontent colors for some reason
+
+
+
+user_pref("browser.newtabpage.enabled", false);
---
+++ .config/thunderbird/user.js
@@ -0,0 +1,13 @@
+// Mozilla User Preferences
+user_pref("calendar.ui.version", 3);
+user_pref("extensions.ui.dictionary.hidden", true);
+user_pref("extensions.ui.extension.hidden", false);
+user_pref("extensions.ui.lastCategory", "addons://list/extension");
+user_pref("extensions.ui.locale.hidden", false);
+user_pref("extensions.ui.theme.hidden", false);
+user_pref("mail.ui-rdf.version", 40);
+user_pref("mail.uidensity", 0);
+user_pref("extensions.webextensions.ExtensionStorageIDB.migrated.addon@darkreader.org", true);
+user_pref("calendar.timezone.useSystemTimezone", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("font.name.sans-serif.x-western", "Hack Nerd Font Mono");
