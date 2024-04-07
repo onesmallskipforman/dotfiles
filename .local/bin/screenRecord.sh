0f6eb410056f81ed77a2adf0c3cdd26df5f36e56
onesmallskipforman <skipper.gonzalez@icloud.com>
2024-04-07 12:12:16 -0400

    Use files under .config/pulse to identify default sink in screenrecord script

--- .local/bin/screenRecord.sh
+++ .local/bin/screenRecord.sh
@@ -1,9 +1,13 @@
 #/bin/sh
 
+# NOTE: for some reason audio recording does not work when using pipewire-media-session on desktop. only wireplumber works
+# pipewire-media-session does work on laptop. not sure why
 function screenRecord() {
-  # local current_sink=$(wpctl status -n | grep '\*.*sink.*vol' | sed 's/^[^\.]*\. \|\[.*//g').monitor
+  # use the name instead of the number because there are inconsistencies in the number between pactl and wpctl
+  # local current_sink=$(wpctl status -n | grep '\*.*alsa_output' | sed 's/^[^\.]*\. \|\[.*//g').monitor
+  local current_sink=$(cat ~/.config/pulse/*-default-sink).monitor
   # local current_sink=$(wpctl status -n | grep '\*.*sink.*vol' | sed 's/^[^0-9]*\|\..*//g')
-  local current_sink=$(wpctl status -n | grep '\*.*alsa_output' | sed 's/^[^0-9]*\|\..*//g')
+  # local current_sink=$(wpctl status -n | grep '\*.*alsa_output' | sed 's/^[^0-9]*\|\..*//g')
   local monitor_opts="$1"
   local RES=$1
   local POS=$2
@@ -11,11 +15,12 @@ function screenRecord() {
   local OUT="$DIR/$(date +%Y_%m_%d--%H-%M-%S_maim).mkv"
   mkdir -p $DIR
   ffmpeg -f x11grab \
-    -framerate 30 \
-    -s $RES -i $POS \
-    -f pulse -i $current_sink \
+    -framerate 60 \
+    -s $RES -probesize 42M -thread_queue_size 4096 -i $POS \
+    -f pulse -thread_queue_size 4096 -i $current_sink \
     $OUT
 }
+# -thread_queue_size 16 \
 
 function screenRecordMonitor() {
   # set -e
