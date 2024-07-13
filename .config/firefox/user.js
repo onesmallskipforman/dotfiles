1807cec6875ecf8eb794ca2a3b5f7028d4d5cbf7
onesmallskipforman <skipper.gonzalez@icloud.com>
2024-07-13 13:09:02 -0400

    Add qutebrowser configs

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -6,19 +6,13 @@ user_pref("extensions.webextensions.ExtensionStorageIDB.migrated.uBlock0@raymond
 // user_pref("xpinstall.signatures.required", false); // don't think this works unless i get ffx dev edition or nightly
 
 
-user_pref("browser.tabs.tabmanager.enabled", false); // remove tab manager button
 
 // 0: Start with a blank page (about:blank).
 // 1: Start with the web page(s) defined as the home page(s). (Default)
 // 2: Load the last visited page.
 // 3: Resume the previous browser session
 user_pref("browser.startup.page", 3);
-
-
-// user_pref("browser.startup.homepage", "about:blank"); // TODO: using about:blank doesn't use usercontent colors for some reason
-user_pref("browser.startup.homepage", "about:newtab");
-
-
-
-user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.startup.homepage", "about:newtab"); // NOTE: using about:blank doesn't use usercontent colors for some reason
+user_pref("browser.newtabpage.enabled", false); // make new tab page blank
 user_pref("browser.tabs.insertAfterCurrent", true);
+user_pref("browser.tabs.tabmanager.enabled", false); // remove tab manager button
