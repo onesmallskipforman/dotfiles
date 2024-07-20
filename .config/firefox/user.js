b256df1bad850a015810e2835692c45f415c4b66
onesmallskipforman <skipper.gonzalez@icloud.com>
2024-07-20 00:01:22 -0400

    Update firefox css

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -1,3 +1,6 @@
+// TODO: goal is to move as much as possible to userchrome css
+
+
 user_pref("browser.uidensity", 1);
 user_pref("browser.compactmode.show", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
@@ -23,4 +26,64 @@ user_pref("browser.tabs.tabmanager.enabled", false); // remove tab manager butto
 user_pref("browser.display.background_color"     , '#282828');
 user_pref("browser.display.background_color.dark", '#282828');
 
-user_pref("browser.tabs.tabMinWidth", '0');
+user_pref("browser.tabs.tabMinWidth", 0);
+
+
+// TODO: doesn't seem to work
+// https://www.reddit.com/r/firefox/comments/z2gz9u/saving_the_customized_toolbar_layout/
+// user_pref("browser.uiCustomization.state", {"placements": {
+//     "widget-overflow-fixed-list":[],
+//     "nav-bar":[
+//         "back-button",
+//         "forward-button",
+//         "stop-reload-button",
+//         "customizableui-special-spring1",
+//         "urlbar-container",
+//         "customizableui-special-spring2",
+//         "save-to-pocket-button",
+//         "downloads-button",
+//         "fxa-toolbar-menu-button",
+//         "unified-extensions-button"
+//     ],
+//     "TabsToolbar":[
+//         // "firefox-view-button",
+//         "tabbrowser-tabs",
+//         "new-tab-button",
+//         "alltabs-button"
+//     ],
+//     "PersonalToolbar":["personal-bookmarks"],
+//     "unified-extensions-area":[
+//         "_news-feed-eradicator-browser-action",
+//         "myallychou_gmail_com-browser-action",
+//         "_contain-facebook-browser-action",
+//         "addon_darkreader_org-browser-action",
+//         "extension_one-tab_com-browser-action",
+//         "ublock0_raymondhill_net-browser-action",
+//         "_88ebde3a-4581-4c6b-8019-2a05a9e3e938_-browser-action",
+//         "_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action"]
+//     },
+//     "seen":[
+//         "save-to-pocket-button",
+//         "_news-feed-eradicator-browser-action",
+//         "myallychou_gmail_com-browser-action",
+//         "_contain-facebook-browser-action",
+//         "addon_darkreader_org-browser-action",
+//         "extension_one-tab_com-browser-action",
+//         "ublock0_raymondhill_net-browser-action",
+//         "_88ebde3a-4581-4c6b-8019-2a05a9e3e938_-browser-action",
+//         "_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action",
+//         "developer-button"
+//     ],
+//     "dirtyAreaCache":["unified-extensions-area"],
+//     "currentVersion":20,
+//     "newElementCount":2
+// });
+
+
+
+// TODO: doesn't seem to work
+// user_pref("browser.newtabpage.activity-stream.showRecentSaves", false);
+// user_pref("browser.newtabpage.activity-stream.showSearch", false);
+// user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+// user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+// user_pref("browser.newtabpage.activity-stream.showWeather", false);
