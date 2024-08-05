49c3a8530bdf88b32ffe32751c7d35ed2dfde712
onesmallskipforman <skipper.gonzalez@icloud.com>
2024-08-05 00:54:01 -0400

    Update bakkesmod script

--- .local/bin/bakkesmod.bash
+++ .local/bin/bakkesmod.bash
@@ -3,18 +3,17 @@
 # Set RL launch options to `"/path/to/this/script.sh" & %command%`
 
 # rocket league wine paths and injector script
-RLDIR=$HOME/.steam/debian-installation/steamapps/compatdata/252950
-WINECFG=$RLDIR/pfx
-C=$WINECFG/drive_c
-BAKKESMOD="$C/Program Files/BakkesMod/BakkesMod.exe"
-PROTON="$(sed -n 4p "$RLDIR"/config_info | xargs -d '\n' dirname)"
+COMPATDATA=$HOME/.steam/debian-installation/steamapps/compatdata/252950
+PROTON=$(sed -n 4p "$COMPATDATA"/config_info | xargs -d '\n' dirname)
+WINEPREFIX=$COMPATDATA/pfx; C=$WINEPREFIX/drive_c
+BAKKESMOD=$C/Program\ Files/BakkesMod/BakkesMod.exe
 
 # Start BakkesMod when RL Starts, either WINEESYNC=1 OR WINEFSYNC=1
 until pgrep -x RocketLeague.ex; do sleep 1; done
-WINEESYNC=1 WINEPREFIX="$WINECFG" "$PROTON"/bin/wine64 "$BAKKESMOD" &
+WINEESYNC=1 WINEPREFIX=$WINEPREFIX $PROTON/bin/wine64 $BAKKESMOD &
 
 # Kill BakkesMod process once initialized
-LOG="$C/users/steamuser/Application Data/bakkesmod/bakkesmod/bakkesmod.log"
+LOG="$C/users/steamuser/AppData/Roaming/bakkesmod/bakkesmod/bakkesmod.log"
 truncate -s0 "$LOG"
 until grep "BakkesMod initialized" "$LOG"; do sleep 1; done
 killall -e BakkesMod.exe
