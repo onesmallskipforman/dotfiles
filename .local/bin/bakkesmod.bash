2f0458dff789e87ee52f11eee5d0ae7178702335
onesmallskipforman <skipper.gonzalez@icloud.com>
2024-08-05 00:58:09 -0400

    Update bakkesmod script

--- .local/bin/bakkesmod.bash
+++ .local/bin/bakkesmod.bash
@@ -4,13 +4,13 @@
 
 # rocket league wine paths and injector script
 COMPATDATA=$HOME/.steam/debian-installation/steamapps/compatdata/252950
-PROTON=$(sed -n 4p "$COMPATDATA"/config_info | xargs -d '\n' dirname)
+PROTON="$(sed -n 4p "$COMPATDATA"/config_info | xargs -d '\n' dirname)"
 WINEPREFIX=$COMPATDATA/pfx; C=$WINEPREFIX/drive_c
-BAKKESMOD=$C/Program\ Files/BakkesMod/BakkesMod.exe
+BAKKESMOD="$C/Program Files/BakkesMod/BakkesMod.exe"
 
 # Start BakkesMod when RL Starts, either WINEESYNC=1 OR WINEFSYNC=1
 until pgrep -x RocketLeague.ex; do sleep 1; done
-WINEESYNC=1 WINEPREFIX=$WINEPREFIX $PROTON/bin/wine64 $BAKKESMOD &
+WINEESYNC=1 WINEPREFIX=$WINEPREFIX "$PROTON"/bin/wine64 "$BAKKESMOD" &
 
 # Kill BakkesMod process once initialized
 LOG="$C/users/steamuser/AppData/Roaming/bakkesmod/bakkesmod/bakkesmod.log"
