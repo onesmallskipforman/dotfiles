1494b86747cd06386daf0ec7490504a205b87fc8
onesmallskipforman <skipper.gonzalez@icloud.com>
2025-06-26 13:02:14 -0400

    Update controls for audio

--- .local/bin/audio.sh
+++ .local/bin/audio.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-
-
-# TODO: eventually would like to get this working with wireplumber,
+# TODO: eventually would like to get this working with wireplumber's wpctl,
 # but there isn't a good equivalent for lising all sinks as a list
 
-
-# pactl list short sinks | awk '{print $2}' | fzf --cycle --reverse | xargs -r pactl set-default-sink
-pactl list short sinks | awk '{print $2}' | rofi -dmenu | xargs pactl set-default-sink
+pactl list short sinks \
+  | awk '{print $2}' \
+  | fzf --cycle --reverse \
+  | xargs pactl set-default-sink
